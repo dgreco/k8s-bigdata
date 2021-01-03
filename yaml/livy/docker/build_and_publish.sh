2f566fe4f9eb1a5add93b7c9c3ea81e481373a08
David Greco <greco@acm.org>
2021-01-03 10:59:22 +0100

    removed livy stuff

--- yaml/livy/docker/build_and_publish.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-../../../scripts/templater.sh Dockerfile.template -s -f ../../../config > Dockerfile
-
-../../../scripts/templater.sh pb.sh.template -s -f ../../../config > pb.sh
-
-sh ./pb.sh
-
-rm ./pb.sh
